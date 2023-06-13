a24323d7689366e6eeee1ba28bc75102cf4c4f18
Felipe <contatofemachado@gmail.com>
2023-06-13 15:41:03 -0300

    criando includes

---
+++ includes/navbar.php
@@ -0,0 +1,32 @@
+<!-- Navigation-->
+<nav style="background-color: var(--bs-cyan)" class="navbar shadow-sm navbar-expand-lg text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+        <a href="index.html"><img src="./img/logo.jpg" alt="Logotipo" width="90" height="55"></a>
+        <button style="background-color: var(--bs-cyan)" class="navbar-toggler text-uppercase shadow font-weight-bold text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-0 mx-lg-1 dropdown">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Jogos
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                        <li><a class="dropdown-item" href="jogoVelha.html">Jogo da velha</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="jogoMemoria.htm">Jogo da memória (NV1)</a></li>
+                        <li><a class="dropdown-item" href="memoriaNv2.htm">Jogo da memória (NV2)</a></li>
+                        <li><a class="dropdown-item" href="memoriaNv3.htm">Jogo da memória (NV3)</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#consumo">Calcular consumo</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#regra3">Regra
+                        de 3</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#ganhosUber">Ganhos na Uber</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
